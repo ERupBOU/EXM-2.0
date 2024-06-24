74560107fc6e8c498ded90c7c42a1031dc9e5ef2
ERupBOU <140595493+ERupBOU@users.noreply.github.com>
2024-06-24 13:30:09 +0700

    Update funk.py

--- mod/funk.py
+++ mod/funk.py
@@ -17,6 +17,7 @@ import os
 import exifread
 import mutagen
 import PyPDF2
+import qrcode
 
 
 
@@ -40,10 +41,21 @@ import PyPDF2
 
 
 
+#========================================
+def generate_qr_code(url, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
 
+    qr.add_data(url)
+    qr.make(fit=True)
 
-
-
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+    print(f"QR-код для '{url}' успешно сохранен в файл '{filename}'.")
 #========================================
 def metapars():
     image_path = input("[?] Путь к файлу > ")
