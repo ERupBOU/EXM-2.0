9f9c573be2575010dbff56847a6908a0743c8058
ERupBOU <140595493+ERupBOU@users.noreply.github.com>
2024-06-24 20:36:41 +0700

    Update EXMM.py

--- EXMM.py
+++ EXMM.py
@@ -74,8 +74,8 @@ print_funk()
 
 
 
-
-while True:
+def mainstart():
+    while True:
     choice = int(input("[?]>>"))
     if choice == 1:
         clear_and_print_logo()
@@ -120,59 +120,7 @@ while True:
         clear_and_print_logo()
         randStringGen()
     elif choice == 15:
-        clear_and_print_logo()
-        currencyConv()
-    elif choice == 16:
-        clear_and_print_logo()
-        whoisLookup()
-    elif choice == 17:
-        clear_and_print_logo()
-        ccardGen()
-    elif choice == 18:
-        clear_and_print_logo()
-        macLookup()
-    elif choice == 19:
-        clear_and_print_logo()
-        tempEmailGen()
-    elif choice == 20:
-        clear_and_print_logo()
-        uaGen()
-    elif choice == 21:
-        clear_and_print_logo()
-        sysInfo()
-    elif choice == 22:
-        clear_and_print_logo()
-        tgParse()
-    elif choice == 23:
-        clear_and_print_logo()
-        vkParse()
-    elif choice == 24:
-        clear_and_print_logo()
-        ocParse()
-    elif choice == 25:
-        clear_and_print_logo()
-        metaData()
-    elif choice == 26:
-        clear_and_print_logo()
-        netData()
-    elif choice == 27:
-        clear_and_print_logo()
-        randDateGen()
-    elif choice == 28:
-        clear_and_print_logo()
-        portScan()
-    elif choice == 29:
-        clear_and_print_logo()
-        ipsteal()
-    elif choice == 30:
-        clear_and_print_logo()
-        cryptFiles()
-    elif choice == 31:
-        clear_and_print_logo()
-        coordLookup()
-    elif choice == 32:
-        clear_and_print_logo()
-        textDistort()
+        clear_and_print_logo()  
     elif choice == 33:
         break
     else:
